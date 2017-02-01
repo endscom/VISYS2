ec18af8983f2fbe988a2aabee35409c8a9bcd50f
ALDER HERNANDEZ <analista.guma@gmail.com>
2017-02-01 14:14:18 -0600

    01-02-2017
    - Correcion Factura vendedor

--- application/views/header/header_login.php
+++ application/views/header/header_login.php
@@ -3,15 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>UNIMARK S.A</title>
-
-
     <!--Import Google Icon Font-->
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="<?PHP echo base_url();?>assets/css/materialize.css"  media="screen,projection"/>
     <link type="text/css" rel="stylesheet" href="<?PHP echo base_url();?>assets/css/styles.css"  media="screen,projection"/>
     <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/img/faviconazul.png" />
-
 </head>
 <body id="contenedorL">
 
